02dd27f03ef152bd0688a5e7b2d96d701a8c3c18
Guillermo Rauch <rauchg@gmail.com>
2010-08-05 16:31:48 -0700

    Added throwError option

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ module.exports = function responseTimeout(options){
     options = options || {};
     options.time = options.time || 8000;
     options.code = options.code || 500;
+    options.throwError = options.throwError || options.throwError === undefined;
     
     return function responseTimeout(req, res, next){
       var writeHead = res.writeHead,
           timer = setTimeout(function(){
-              res.writeHead(options.code);
-              res.end();
+              if (options.throwError){
+                next(new Error('Timeout'));
+              } else {
+                res.writeHead(options.code);
+                res.end();
+              }
           }, options.time);
       
       req.clearTimeout = function(){
